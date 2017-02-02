a2e7ddeb1338653a7df99179c66280d08f8ac0ff
benkio <benkio89@gmail.com>
2017-02-02 14:08:33 +0100

    add a function for the currying example

--- src/HigherOrderFunction.hs
+++ src/HigherOrderFunction.hs
@@ -7,5 +7,8 @@ applySum :: Int -> Int -> Int
 applySum x y = let f = algebraApplicator (+)
                in  f x y
 
+applySum' :: Int -> (Int -> Int)
+applySum' x = \y -> x + y
+
 ifThenElse :: (a -> b) -> (a -> b) -> Bool -> (a -> b)
 ifThenElse f g c = if c then f else g
