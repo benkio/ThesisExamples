04f79cc5ff07b8032e2107cce5bfaebe6e4137e5
benkio <benkio89@gmail.com>
2017-02-12 16:27:50 +0100

    add the applicative and functor for state

--- js/applicative.js
+++ js/applicative.js
@@ -20,6 +20,8 @@ function Applicative() {
     this.apply = function(liftedFunc){};
 }
 
+/******************** ARRAY APPLICATIVE ****************************/
+
 Array.prototype.pure  = function(a){ return [a]; }; // To be explicit, we can simply use [a] instead of [].pure(a).
 Array.prototype.apply = function(arrayOfFunc) {
     if (arrayOfFunc.length == 0) return [];
@@ -49,3 +51,38 @@ console.log("The type applicative must have a way to lift a sigle value, eg in l
 console.log("With applicative we can easily apply a list of function to a list of value(same list context): " + arrayExample.apply(arrayOfFunc1) );
 
 console.log("Finally we can chain computations, applying other list of functions: " + arrayExample.apply(arrayOfFunc1).apply(arrayOfFunc2) );
+
+ 
+/*********************** STATE APPLICATIVE *******************************************/
+
+State.prototype.pure = function(a){ return new State(function(state){ return [a, state]; }); };
+State.prototype.apply = function(stateFunc){
+    var outStateObject = this;
+    return new State(function(state) {
+        var [f, s1] = stateFunc.execState(state);
+        var [v, s2] = outStateObject.execState(state);
+        var newValue = f(v);
+        return [newValue, s2];
+    });
+};
+
+/*
+
+  don't works for the same reason of the functor law for state
+assert(isEquivalent(stack.apply(stack.pure(id)), id(stack)), "Applicative Identity Law");
+assert(isEquivalent(stack.pure(2).apply(stack.pure(addOne)), stack.pure(addOne(2))), "Homomorphysm Law");
+assert(isEquivalent(stack.pure(2).apply(stack.pure(addOne)), stack.pure(addOne).apply(stack.pure(functionApplication(2)))), "Interchange Law" );
+assert(isEquivalent(stack.pure(2).apply(stack.pure(addOne)).apply(stack.pure(square)),
+                    stack.pure(2).apply(stack.pure(addOne).apply(stack.pure(square).apply(stack.pure(functionComposition))))), "Associativity Law");
+
+*/
+/********************* APPLICATIVE EXAMPLE ***********************************/
+
+console.log("---------Applicative example state-------------");
+
+console.log("The type applicative must have a way to lift a sigle value, eg in state from 2 to: " + JSON.stringify( stack.pure(2).execState([0])  ));
+
+debugger;
+console.log("With applicative we can easily apply a state of function to a state of value(same state context): " + JSON.stringify( stack.apply(stack.pure(addOne)).execState([0]) ));
+
+console.log("Finally we can chain computations, applying other state of functions: " + JSON.stringify(stack.pure(2).apply(stack.pure(addOne)).apply(stack.pure(square)).execState([0])) );
--- js/functor.js
+++ js/functor.js
@@ -12,16 +12,6 @@ function Functor () {
     this.fmap  = function(purefunc){};
 };
 
-Array.prototype.fmap = function (purefunc) {
-    if (this.length == 0) return [];
-    var result = this.slice(1);
-    result = result.fmap(purefunc);
-    result.unshift(purefunc(this[0]));
-    return result;
-};
-
-/******************** LIST FUNCTOR EXAMPLE ******************************/
-
 
 "use strict";
 
@@ -37,20 +27,69 @@ function multByTwo(a) { return a * 2; };
 function square (a){ return a * a; };
 function squareAndAddOne (a){ return addOne(square(a)); };
 
+/******************** LIST FUNCTOR EXAMPLE ******************************/
+
 var arrayExample = [2,4];
+
+Array.prototype.fmap = function (purefunc) {
+    if (this.length == 0) return [];
+    var result = this.slice(1);
+    result = result.fmap(purefunc);
+    result.unshift(purefunc(this[0]));
+    return result;
+};
 
 /******************* FUNCTOR LAW PROOF ************************************/
 
-assert(isEquivalent(arrayExample.fmap(id), id(arrayExample)), "law 1");
+assert(isEquivalent(arrayExample.fmap(id), id(arrayExample)), "law 1 array");
 
-assert(isEquivalent(arrayExample.fmap(squareAndAddOne), arrayExample.fmap(square).fmap(addOne)), "law 2");
+assert(isEquivalent(arrayExample.fmap(squareAndAddOne), arrayExample.fmap(square).fmap(addOne)), "law 2 array");
 
 /********************* FUNCTOR EXAMPLE ***********************************/
 
 console.log("Initial Array value: " + arrayExample);
 
-console.log("---------Functor example-------------")
+console.log("---------Functor Example Array-------------")
 
 console.log("Functor value after addOne and Square functions: " + arrayExample.fmap(addOne).fmap(square));
 
 console.log("Same previous result using directly the map operation: " + arrayExample.map(addOne).map(square));
+
+/******************* STATE FUNCTOR ***************************************/
+
+var stack = new State(function(initialStack){ return [1, initialStack]; } );
+
+function State(stateFunc){
+    this.stateFunc = stateFunc;
+    this.fmap = function(func){ //with fmap there not effects on the state, it's only passed along the computation
+        return (new State(function(state){
+            var [a, newState]   = stateFunc(state);
+            var newValue = func(a);
+            return [newValue, newState]
+        }))
+    };
+    this.execState = function(state) {
+        return this.stateFunc(state);
+    };
+};
+
+/******************** FUNCTOR LAW PROOF ********************************/
+
+/*
+
+  don't work because it's difficult to compare two states.
+
+assert(isEquivalent(stack.fmap(id), id(stack)), "law 1 state");
+
+assert(isEquivalent(stack.fmap(squareAndAddOne), stack.fmap(square).fmap(addOne)), "law 2 state");
+*/
+
+/******************* STATE FUNCTOR EXAMPLE *******************************/
+
+console.log("Initial State value: " + JSON.stringify(stack.execState([0])));
+
+console.log("---------Functor Example State-------------")
+
+var stackAfterFmap = stack.fmap(square).fmap(addOne);
+
+console.log("state functor applyied to two functions and executed: " + JSON.stringify(stackAfterFmap.execState([0])) );
